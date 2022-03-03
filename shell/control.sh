408d5dde99e7582711c070c5f2fc187b6f4bb6cb
Super Manito <68613938+SuperManito@users.noreply.github.com>
2022-03-03 20:13:00 +0800

    Update

--- shell/control.sh
+++ shell/control.sh
@@ -307,6 +307,7 @@ function Bot_Control() {
         echo -e "$WORKING 开始安装模块...\n"
         cp -rf $BotRepoDir/jbot $RootDir
         cd $BotDir
+        pip3 config set global.index-url https://mirrors.aliyun.com/pypi/simple/
         pip3 --default-timeout=300 install -r requirements.txt
         pip3 install aiohttp Telethon==1.24.0
         if [[ $? -eq 0 ]]; then
