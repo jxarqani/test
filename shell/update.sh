7bf58c110d8693ae63136113e39307d29f58ac9b
Super Manito <68613938+SuperManito@users.noreply.github.com>
2022-06-19 22:36:51 +0800

    Update

--- shell/update.sh
+++ shell/update.sh
@@ -771,11 +771,6 @@ function Update_Designated() {
     fi
     ## 判定是否存在仓库
     if [ -d ${AbsolutePath}/.git ]; then
-        local _tmp1="${AbsolutePath}/$(b amRfNjE4X3JlZC5qcwo=)"
-        local _tmp2="${AbsolutePath}/$(b amRfNjE4cmVkLmpzCg==)"
-        [ -f $_tmp1 ] && rm -rf $_tmp1 >/dev/null 2>&1
-        [ -f $_tmp2 ] && rm -rf $_tmp2 >/dev/null 2>&1
-
         if [[ "${AbsolutePath}" = "$RootDir" ]]; then
             Title "shell"
             Update_Shell
