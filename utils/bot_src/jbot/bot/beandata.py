0e5e3ecc8daebb13bbe0dcd35f5ee7e8ef3cc539
Dellear <358009775@qq.com>
2022-08-27 23:24:12 +0800

    fix /bean /chart

--- utils/bot_src/jbot/bot/beandata.py
+++ utils/bot_src/jbot/bot/beandata.py
@@ -13,15 +13,12 @@ requests.adapters.DEFAULT_RETRIES = 5
 session = requests.session()
 session.keep_alive = False
 
-url = "https://api.m.jd.com/api"
-
+url = "https://api.m.jd.com/client.action"
 
 def gen_body(page):
     body = {
-        "beginDate": datetime.datetime.utcnow().replace(tzinfo=timezone.utc).astimezone(SHA_TZ).strftime("%Y-%m-%d %H:%M:%S"),
-        "endDate": datetime.datetime.utcnow().replace(tzinfo=timezone.utc).astimezone(SHA_TZ).strftime("%Y-%m-%d %H:%M:%S"),
-        "pageNo": page,
-        "pageSize": 20,
+        "page": str(page),
+        "pageSize": "20",
     }
     return body
 
@@ -29,13 +26,8 @@ def gen_body(page):
 def gen_params(page):
     body = gen_body(page)
     params = {
-        "functionId": "jposTradeQuery",
-        "appid": "swat_miniprogram",
-        "client": "tjj_m",
-        "sdkName": "orderDetail",
-        "sdkVersion": "1.0.0",
-        "clientVersion": "3.1.3",
-        "timestamp": int(round(time.time() * 1000)),
+        "functionId": "getJingBeanBalanceDetail",
+        "appid": "ld",
         "body": json.dumps(body)
     }
     return params
@@ -48,12 +40,10 @@ def get_beans_7days(ck):
         headers = {
             "Host": "api.m.jd.com",
             "Connection": "keep-alive",
-            "charset": "utf-8",
-            "User-Agent": "Mozilla/5.0 (Linux; Android 10; MI 9 Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/78.0.3904.62 XWEB/2797 MMWEBSDK/201201 Mobile Safari/537.36 MMWEBID/7986 MicroMessenger/8.0.1840(0x2800003B) Process/appbrand4 WeChat/arm64 Weixin NetType/4G Language/zh_CN ABI/arm64 MiniProgramEnv/android",
-            "Content-Type": "application/x-www-form-urlencoded;",
-            "Accept-Encoding": "gzip, compress, deflate, br",
+            "User-Agent": "jdapp;iPhone;9.4.4;14.3;network/4g;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1",
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Accept-Encoding": "gzip,deflate",
             "Cookie": ck,
-            "Referer": "https://servicewechat.com/wxa5bf5ee667d91626/141/page-frame.html",
         }
         days = []
         for i in range(0, 7):
@@ -63,47 +53,45 @@ def get_beans_7days(ck):
         beans_out = {key: 0 for key in days}
         while day_7:
             page = page + 1
-            resp = session.get(url, params=gen_params(page),
-                               headers=headers, timeout=100).text
-            res = json.loads(resp)
-            if res['resultCode'] == 0:
-                for i in res['data']['list']:
+            res = session.post(url=url, headers=headers, data=gen_params(page)).json()
+            if res['code'] == '0':
+                for i in res['detailList']:
                     for date in days:
-                        if str(date) in i['createDate'] and i['amount'] > 0:
+                        if str(date) in i['date'] and int(i['amount']) > 0:
                             beans_in[str(date)] = beans_in[str(
-                                date)] + i['amount']
+                                date)] + int(i['amount'])
                             break
-                        elif str(date) in i['createDate'] and i['amount'] < 0:
+                        elif str(date) in i['date'] and int(i['amount']) < 0:
                             beans_out[str(date)] = beans_out[str(
-                                date)] + i['amount']
+                                date)] + int(i['amount'])
                             break
-                    if i['createDate'].split(' ')[0] not in str(days):
+                    if i['date'].split(' ')[0] not in str(days):
                         day_7 = False
             else:
                 return {'code': 400, 'data': res}
+        days = list(map(lambda x: x[5:], days))
         return {'code': 200, 'data': [beans_in, beans_out, days]}
     except Exception as e:
         logger.error(str(e))
         return {'code': 400, 'data': str(e)}
 
-
 def get_total_beans(ck):
     try:
         headers = {
-            "Host": "wxapp.m.jd.com",
+            "Host": "me-api.jd.com",
             "Connection": "keep-alive",
-            "charset": "utf-8",
             "User-Agent": "Mozilla/5.0 (Linux; Android 10; MI 9 Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/78.0.3904.62 XWEB/2797 MMWEBSDK/201201 Mobile Safari/537.36 MMWEBID/7986 MicroMessenger/8.0.1840(0x2800003B) Process/appbrand4 WeChat/arm64 Weixin NetType/4G Language/zh_CN ABI/arm64 MiniProgramEnv/android",
             "Content-Type": "application/x-www-form-urlencoded;",
-            "Accept-Encoding": "gzip, compress, deflate, br",
+            "Accept-Encoding": "gzip,deflate",
             "Cookie": ck,
         }
-        jurl = "https://wxapp.m.jd.com/kwxhome/myJd/home.json"
+        jurl = "https://me-api.jd.com/user_new/info/GetJDUserInfoUnion"
         resp = session.get(jurl, headers=headers, timeout=100).text
         res = json.loads(resp)
-        return res['user']['jingBean']
+        return res['data']['assetInfo']['beanNum'], res['data']['userInfo']['baseInfo']['nickname'], res['data']['userInfo']['baseInfo']['headImageUrl']
     except Exception as e:
         logger.error(str(e))
+
 
 def get_bean_data(i):
     try:
@@ -112,7 +100,7 @@ def get_bean_data(i):
         if cookies:
             ck = cookies[i-1]
             beans_res = get_beans_7days(ck)
-            beantotal = get_total_beans(ck)
+            beantotal, nickname, pic = get_total_beans(ck)
             if beans_res['code'] != 200:
                 return beans_res
             else:
@@ -122,8 +110,10 @@ def get_bean_data(i):
                     beantotal = int(
                         beantotal) - int(beans_res['data'][0][i]) - int(beans_res['data'][1][i])
                     beans_in.append(int(beans_res['data'][0][i]))
-                    beans_out.append(int(str(beans_res['data'][1][i]).replace('-', '')))
+                    beans_out.append(
+                        int(str(beans_res['data'][1][i]).replace('-', '')))
                     beanstotal.append(beantotal)
-            return {'code': 200, 'data': [beans_in[::-1], beans_out[::-1], beanstotal[::-1], beans_res['data'][2][::-1]]}
+            return {'code': 200, 'data': [beans_in[::-1], beans_out[::-1], beanstotal[::-1], beans_res['data'][2][::-1], nickname, pic]}
     except Exception as e:
         logger.error(str(e))
+        return {"code": 400}
--- utils/bot_src/jbot/bot/chart.py
+++ utils/bot_src/jbot/bot/chart.py
@@ -8,6 +8,7 @@ from telethon import events
 from .. import jdbot, chat_id, LOG_DIR, logger, BOT_DIR, ch_name, BOT_SET
 from ..bot.utils import CONFIG_SH_FILE, get_cks
 from ..bot.quickchart import QuickChart, QuickChartFunction
+from .beandata import get_bean_data
 
 period = 10  # 消息自动删除的时间 单位：秒
 
@@ -19,115 +20,7 @@ requests.adapters.DEFAULT_RETRIES = 5
 session = requests.session()
 session.keep_alive = False
 
-url = "https://api.m.jd.com/client.action"
-
-def gen_body(page):
-    body = {
-        "page": str(page),
-        "pageSize": "20",
-    }
-    return body
-
-
-def gen_params(page):
-    body = gen_body(page)
-    params = {
-        "functionId": "getJingBeanBalanceDetail",
-        "appid": "ld",
-        "body": json.dumps(body)
-    }
-    return params
-
-
-def get_beans_7days(ck):
-    try:
-        day_7 = True
-        page = 0
-        headers = {
-            "Host": "api.m.jd.com",
-            "Connection": "keep-alive",
-            "User-Agent": "jdapp;iPhone;9.4.4;14.3;network/4g;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1",
-            "Content-Type": "application/x-www-form-urlencoded",
-            "Accept-Encoding": "gzip,deflate",
-            "Cookie": ck,
-        }
-        days = []
-        for i in range(0, 7):
-            days.append(
-                (datetime.date.today() - datetime.timedelta(days=i)).strftime("%Y-%m-%d"))
-        beans_in = {key: 0 for key in days}
-        beans_out = {key: 0 for key in days}
-        while day_7:
-            page = page + 1
-            res = session.post(url=url, headers=headers, data=gen_params(page)).json()
-            if res['code'] == '0':
-                for i in res['detailList']:
-                    for date in days:
-                        if str(date) in i['date'] and int(i['amount']) > 0:
-                            beans_in[str(date)] = beans_in[str(
-                                date)] + int(i['amount'])
-                            break
-                        elif str(date) in i['date'] and int(i['amount']) < 0:
-                            beans_out[str(date)] = beans_out[str(
-                                date)] + int(i['amount'])
-                            break
-                    if i['date'].split(' ')[0] not in str(days):
-                        day_7 = False
-            else:
-                return {'code': 400, 'data': res}
-        days = list(map(lambda x: x[5:], days))
-        return {'code': 200, 'data': [beans_in, beans_out, days]}
-    except Exception as e:
-        logger.error(str(e))
-        return {'code': 400, 'data': str(e)}
-
-
-def get_total_beans(ck):
-    try:
-        headers = {
-            "Host": "me-api.jd.com",
-            "Connection": "keep-alive",
-            "User-Agent": "Mozilla/5.0 (Linux; Android 10; MI 9 Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/78.0.3904.62 XWEB/2797 MMWEBSDK/201201 Mobile Safari/537.36 MMWEBID/7986 MicroMessenger/8.0.1840(0x2800003B) Process/appbrand4 WeChat/arm64 Weixin NetType/4G Language/zh_CN ABI/arm64 MiniProgramEnv/android",
-            "Content-Type": "application/x-www-form-urlencoded;",
-            "Accept-Encoding": "gzip,deflate",
-            "Cookie": ck,
-        }
-        jurl = "https://me-api.jd.com/user_new/info/GetJDUserInfoUnion"
-        resp = session.get(jurl, headers=headers, timeout=100).text
-        res = json.loads(resp)
-        return res['data']['assetInfo']['beanNum'], res['data']['userInfo']['baseInfo']['nickname'], res['data']['userInfo']['baseInfo']['headImageUrl']
-    except Exception as e:
-        logger.error(str(e))
-
-
-def get_bean_data(i):
-    try:
-        ckfile = CONFIG_SH_FILE
-        cookies = get_cks(ckfile)
-        if cookies:
-            ck = cookies[i-1]
-            beans_res = get_beans_7days(ck)
-            beantotal, nickname, pic = get_total_beans(ck)
-            if beans_res['code'] != 200:
-                return beans_res
-            else:
-                beans_in, beans_out = [], []
-                beanstotal = [int(beantotal), ]
-                for i in beans_res['data'][0]:
-                    beantotal = int(
-                        beantotal) - int(beans_res['data'][0][i]) - int(beans_res['data'][1][i])
-                    beans_in.append(int(beans_res['data'][0][i]))
-                    beans_out.append(
-                        int(str(beans_res['data'][1][i]).replace('-', '')))
-                    beanstotal.append(beantotal)
-            return {'code': 200, 'data': [beans_in[::-1], beans_out[::-1], beanstotal[::-1], beans_res['data'][2][::-1], nickname, pic]}
-    except Exception as e:
-        logger.error(str(e))
-        return {"code": 400}
-
-
 BEAN_IMG = f'{LOG_DIR}/TelegramBot/bean.png'
-
 
 def createpic(text, totalbean, avatar_url="https://img11.360buyimg.com/jdphoto/s120x120_jfs/t21160/90/706848746/2813/d1060df5/5b163ef9N4a3d7aa6.png"):
     fontSize = 60
